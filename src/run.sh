07d3690580e0796b48f8ab5f41f0aeaa90d3ae7f
Eric Lief <liefe@u-pl10.ms.mff.cuni.cz>
2018-02-22 22:04:02 +0100

    cuckoo edit

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 javac -d ../bin *.java
-#ulimit -t unlimited && nice -n 19 java -cp ../bin CuckooTabRandTest
-#ulimit -t unlimited && nice -n 19 java -cp ../bin CuckooMultShiftRandTest
+ulimit -t unlimited && nice -n 19 java -cp ../bin CuckooTabRandTest
+ulimit -t unlimited && nice -n 19 java -cp ../bin CuckooMultShiftRandTest
 #ulimit -t unlimited && nice -n 19 java -cp ../bin LPModRandTest
 #ulimit -t unlimited && nice -n 19 java -cp ../bin LPMultShiftRandTest
 #ulimit -t unlimited && nice -n 19 java -cp ../bin LPTabRandTest
-ulimit -t unlimited && nice -n 19 java -cp ../bin LPMultShiftSeqTest
+#ulimit -t unlimited && nice -n 19 java -cp ../bin LPMultShiftSeqTest
 #ulimit -t unlimited && nice -n 19 java -cp ../bin LPTabSeqTest
